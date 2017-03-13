e2ad02d3a295ad52efec7f66e7d680ebe5fc273c
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2017-03-13 11:23:36 -0500

    created footer. styled footer

---
+++ footer.php
@@ -0,0 +1,38 @@
+
+<footer>
+	<h4>Let's chat!</h4>
+
+	<div>
+		<a href="mailto:hello@jqdesign.co" target="_blank">
+			<i class="fa fa-envelope fa-2x" aria-hidden="true"></i></a>
+		<a href="http://www.instagram.com/qjac/" target="_blank">
+			<i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+		<a href="http:/www.twitter.com/Q_Quesadilla" target="_blank">
+			<i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+		<a href="http:/www.github.com/qjac" target="_blank">
+			<i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
+		<a href="http:/www.linkedin.com/in/jacquintanilla/" target="_blank">
+			<i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
+	</div>
+
+	<p>Website designed and developed by Jacqueline Quintanilla. &copy; <?php echo date("Y"); ?>
+	</p>
+</footer>
+
+
+<!--   <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script> -->
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. UA-93102704-1 -->
+<script>
+	(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+		function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+		e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+		e.src='https://www.google-analytics.com/analytics.js';
+		r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+	ga('create','UA-93102704-1','auto');ga('send','pageview');
+</script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,19 @@
+<?php $title="Design + Web Development";
+		$pageClass="index";?>
+
+<?php include ("header.php"); ?>
+
+
+
+
+        <main> 
+            <img src="img/logo.svg" alt="JQ.">
+            <div class="soon">
+                <h1>Coming Soon!</h1>
+                <p>Until then, drop a line at hello@jqdesign.co</p>
+            </div>
+        </main>
+
+        <?php include ("footer.php"); ?>
+
+<!--    body closes in footer.php-->
