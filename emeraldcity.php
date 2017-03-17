acc70015da7692ebc91f980752bb662182af2263
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2017-03-17 02:52:38 -0500

    created and styled portfolio details  page

---
+++ emeraldcity.php
@@ -0,0 +1,43 @@
+<?php $title="Emerald City";
+		$pageClass="portfolio-item emeraldcity";?>
+
+<?php include ("header.php"); ?>
+
+
+
+
+        <main class="content">
+	       <section class="content-top">
+<!--		       h1 is for project title-->
+		       <h1>&nbsp;Design&nbsp;<span class="plus">+</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <br> Web&nbsp;Development</h1>
+		       <hr>
+	       </section>
+
+	        <section class="project-intro">
+		        <h2>brief brand statement </h2>
+		        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content </p>
+		        <div class="btn"><a href="#" class="hvr-icon-forward">Visit Site </a></div>
+	        </section>
+
+
+	        <section class="portfolio-details">
+		        <img  src="http://placecage.com/c/600/600" alt="desc" class="full-width">
+
+		        <img  src="http://placecage.com/c/600/400" alt="desc" class="half-width">
+		        <img  src="http://placecage.com/c/600/400" alt="desc" class="half-width">
+
+		        <img  src="http://placecage.com/c/600/400" alt="desc" class="full-width">
+
+		        <img  src="http://placecage.com/c/600/400" alt="desc" class="full-width">
+	        </section>
+
+	        <section>
+		        <p class="category">identity // packaging // advertising // web design // front end development</p>
+	        </section>
+
+
+        </main>
+
+        <?php include ("footer.php"); ?>
+
+<!--    body closes in footer.php-->
--- index.php
+++ index.php
@@ -3,17 +3,15 @@
 
 <?php include ("header.php"); ?>
 
-
-
-
         <main class="content">
 	       <section class="content-top">
 		       <h1>&nbsp;Design&nbsp;<span class="plus">+</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <br> Web&nbsp;Development</h1>
 		       <hr>
 	       </section>
+
         <section class="portfolio">
 		        <figure class="portfolio-item">
-			        <a href="#" class="hvr-trim">
+			        <a href="emeraldcity.php" class="hvr-trim">
 				        <img  src="http://placecage.com/c/600/600" alt="desc" class="featureimg">
 			        </a>
 			        <figcaption>
@@ -21,7 +19,7 @@
 				        <p class="category">identity // packaging // advertising // web design // front end development</p>
 				        <p>Why do we use it?
 				           It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-				        <div class="btn "><a href="#" class="hvr-icon-forward">Take a closer look! </i></a></div>
+				        <div class="btn "><a href="emeraldcity.php" class="hvr-icon-forward">Take a closer look! </a></div>
 			        </figcaption>
 		        </figure>
 
@@ -35,7 +33,7 @@
 			        <p class="category">identity // packaging // advertising // web design // front end development</p>
 			        <p>Why do we use it?
 			           It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-			        <div class="btn "><a href="#" class="hvr-icon-forward">Take a closer look! </i></a></div>
+			        <div class="btn "><a href="#" class="hvr-icon-forward">Take a closer look! </a></div>
 		        </figcaption>
 	        </figure>
 
@@ -49,7 +47,7 @@
 			        <p class="category">identity // packaging // advertising // web design // front end development</p>
 			        <p>Why do we use it?
 			           It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-			        <div class="btn "><a href="#" class="hvr-icon-forward">Take a closer look! </i></a></div>
+			        <div class="btn "><a href="#" class="hvr-icon-forward">Take a closer look! </a></div>
 		        </figcaption>
 	        </figure>
 
@@ -63,7 +61,7 @@
 			        <p class="category">identity // packaging // advertising // web design // front end development</p>
 			        <p>Why do we use it?
 			           It is a long established fact that a reader will be distracted by the readable content of a page when looking at itlved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-			        <div class="btn "><a href="#" class="hvr-icon-forward">Take a closer look! </i></a></div>
+			        <div class="btn "><a href="#" class="hvr-icon-forward">Take a closer look! </a></div>
 		        </figcaption>
 	        </figure>
 
