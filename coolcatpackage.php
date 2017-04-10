c37d2b226be14ea17899f28bf2da2fe73a7d6692
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2017-04-09 23:05:29 -0500

    added content to page. adjusted styling on hr

--- coolcatpackage.php
+++ coolcatpackage.php
@@ -15,7 +15,6 @@
 	       </section>
 
 	        <section class="project-intro">
-
 		        <h2>Great in the Sore. Better at Home.</h2>
 		        <p>Designed to go from retail to the home, this package keeps the coasters protected and looking good long after the sale. Featuring bold type to catch attention on busy shelves, the box really shines when it’s opened. That quirky cat greets us with an inner lining and the asymmetrical lid suddenly makes sense when you store it under the base, turning the entire package into a sturdy and attractive storage tray with a convenient thumb notch to keep your coasters at the ready. You never know when a party might spring up!</p>
 
@@ -27,36 +26,46 @@
 			        <img sizes="100vw" srcset="img/ccc-pack/ccc-package-480.jpg 480w, img/ccc-pack/ccc-package-960.jpg 960w, img/ccc-pack/ccc-package-1920.jpg 1920w" src="img/ccc-pack/ccc-package-960.jpg" alt="package on display" class="full-width">
 		        </div>
 		        <div class="row">
-			        <img sizes="50vw" srcset="img/ccc-pack/view2-480.jpg 480w,img/ccc-pack/view2-960.jpg 960w, img/ccc-pack/view2-1920.jpg 1920w" src="img/ccc-pack/view2-960.jpg" alt="sketches of draft logos" class="half-width">
-			        <img sizes="50vw" srcset="img/ccc-pack/view1-480.png 480w, img/ccc-pack/view1-960.png 960w, img/ccc-pack/view1-1920.png 1920w" src="img/ccc-pack/view1-960.png" alt="moodboard" class="half-width">
+			        <img sizes="50vw" srcset="img/ccc-pack/ccc-package-view1-480.jpg 480w, img/ccc-pack/ccc-package-view1-960.jpg 960w, img/ccc-pack/ccc-package-view1-1920.jpg 1920w" src="img/ccc-pack/ccc-package-view1-960.jpg" alt="package set up for table top storage for coasters" class="half-width">
+			        <img sizes="50vw" srcset="img/ccc-pack/ccc-package-view2-480.jpg 480w,img/ccc-pack/ccc-package-view2-960.jpg 960w, img/ccc-pack/ccc-package-view2-1920.jpg 1920w" src="img/ccc-pack/ccc-package-view2-960.jpg" alt="view of bottom of package and lid lining" class="half-width">
 		        </div>
 	        </section>
+
 	        <section class="project-intro">
-		        <p>Catering to the aficionados and to the newly introduced, Emerald City needed a brand identity that was classy and welcoming. The choice of green is an obvious nod to the plant at the core of the business, the yellow brings sunlight and cheer, and white keeps everything airy and open. The logo harkens back to classic bakeries of yore with graceful swoops while geometric shapes keep a crisp, modern vibe.</p>
+		        <p>The asymmetrical package required engineering and prototyping to perfect. The thumb notch needed to be large enough to be useful and the lid needed to cover the notch completely while on store shelves, but not get in the way while in use as a tray at home.</p>
 		        <hr>
 	        </section>
+
 	        <section class="project-img">
+		        <div class="row">
+			        <img sizes="50vw" srcset="img/ccc-pack/ccc-package-cut1-480.jpg 480w, img/ccc-pack/ccc-package-cut1-960.jpg 960w, img/ccc-pack/ccc-package-cut1-1920.jpg 1920w" src="img/ccc-pack/ccc-package-cut1-960.jpg" alt="cut and scored prototype in progress" class="half-width">
+			        <img sizes="50vw" srcset="img/ccc-pack/ccc-package-cut2-480.jpg 480w, img/ccc-pack/ccc-package-cut2-960.jpg 960w, img/ccc-pack/ccc-package-cut2-1920.jpg 1920w" src="img/ccc-pack/ccc-package-cut2-960.jpg" alt="drafting box on paperboard" class="half-width">
+		        </div>
+		        <div class="row">
+			        <img sizes="100vw" srcset="img/ccc-pack/ccc-package-cut-480.jpg 480w, img/ccc-pack/ccc-package-cut-960.jpg 960w, img/ccc-pack/ccc-package-cut-1920.jpg 1920w" src="img/ccc-pack/ccc-package-cut-960.jpg" alt="assembled first round prototype with alterations marked" class="full-width">
+		        </div>
+	        </section>
 
+<!--	        <section class="project-intro">-->
+<!--		        <p>The asymmetrical package required engineering and prototyping to perfect. The thumb notch needed to be large enough to be useful and the lid needed to cover the notch completely while on store shelves, but not get in the way while in use as a tray at home.</p>-->
+<!--		        <hr>-->
+<!--	        </section>-->
 
+	        <section class="project-img">
 		        <div class="row">
-			        <img sizes="50vw" srcset="img/ec/ec-hoodie-close-192.jpg 192w, img/ec/ec-hoodie-close-480.jpg 480w, img/ec/ec-hoodie-close-960.jpg 960w, img/ec/ec-hoodie-close-1920.jpg 1920w" src="img/ec/ec-hoodie-close-960.jpg" alt="green zip-up sweatshirt" class="half-width">
-			        <img sizes="50vw" srcset="img/ec/ec-tee-192.jpg 192w, img/ec/ec-tee-480.jpg 480w, img/ec/ec-tee-960.jpg 960w, img/ec/ec-tee-1920.jpg 1920w" src="img/ec/ec-tee-960.jpg" alt="yellow t-shirt" class="half-width">
+			        <img sizes="50vw" srcset="img/ccc-pack/ccc-package-digitalroughs-480.jpg 480w, img/ccc-pack/ccc-package-digitalroughs-960.jpg 960w, img/ccc-pack/ccc-package-digitalroughs-1920.jpg 1920w" src="img/ccc-pack/ccc-package-digitalroughs-960.jpg" alt="digital roughs of package front" class="half-width">
+			        <img sizes="50vw" srcset="img/ccc-pack/ccc-package-drafts-480.jpg 480w, img/ccc-pack/ccc-package-drafts-960.jpg 960w, img/ccc-pack/ccc-package-drafts-1920.jpg 1920w" src="img/ccc-pack/ccc-package-drafts-960.jpg" alt="drafts of box graphics" class="half-width">
 		        </div>
-		        <div class="row">
-			        <img sizes="100vw" srcset="img/ec/ec-web-192.jpg 192w, img/ec/ec-web-480.jpg 480w, img/ec/ec-web-960.jpg 960w, img/ec/ec-web-1920.jpg 1920w" src="img/ec/ec-web-960.jpg" alt="website displayed on desktop, tablet, and mobile devices" class="full-width">
-		        </div>
-		        <div class="row">
-			        <img src="img/ec/ec-ad1.jpg" alt="digital ad" class="no-stretch">
-			        <img src="img/ec/ec-ad2.jpg" alt="digital ad" class="no-stretch">
-		        </div>
-		        <div class="row">
-			        <img sizes="100vw" srcset="img/ec/ec-icon-480.png 480w, img/ec/ec-icon-960.png 960w, img/ec/ec-icon-1920.png 1920w" src="img/ec/ec-icon-960.png" alt="E icon" class="full-width">
-		        </div>
+	        </section>
 
+	        <section class="project-img">
+		        <div class="row">
+			        <img sizes="100vw" srcset="img/ccc-pack/ccc-package-wood-480.jpg 480w, img/ccc-pack/ccc-package-wood-960.jpg 960w, img/ccc-pack/ccc-package-wood-1920.jpg 1920w" src="img/ccc-pack/ccc-package-wood-960.jpg" alt="closed package on display" class="full-width">
+		        </div>
 	        </section>
 
 	        <section class="project-intro">
-		        <p class="category">identity // digital&nbsp;advertising // WordPress&nbsp;development</p>
+		        <p class="category">package development //package design // art direction</p>
 <!--		        <p>Tools used: Adobe Illustrator + Adobe Animate + WordPress</p>-->
 	        </section>
 
