a52d92f3a36d522dca52fbcbbddf4ad5edaad50a
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2017-04-11 12:52:59 -0500

    clean urls and link social media

--- footer.php
+++ footer.php
@@ -5,25 +5,25 @@
 	<div>
 		<a href="mailto:hello@jqdesign.co" target="_blank">
 			<i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i></a>
-		<a href="http:/www.github.com/qjac" target="_blank">
+		<a href="https://github.com/qjac" target="">
 			<i class="fa fa-github fa-2x" aria-hidden="true"></i></a>
-		<a href="http://www.instagram.com/qjac/" target="_blank">
+		<a href="https://www.instagram.com/qjac" target="">
 			<i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
-		<a href="http:/www.twitter.com/Q_Quesadilla" target="_blank">
+		<a href="https://www.twitter.com/Q_Quesadilla" target="">
 			<i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
-		<a href="http:/www.linkedin.com/in/jacquintanilla/" target="_blank">
+		<a href="https://www.linkedin.com/in/jacquintanilla/" target="">
 			<i class="fa fa-linkedin fa-2x" aria-hidden="true"></i></a>
 	</div>
 
-	<p>Website designed and developed by Jacqueline Quintanilla. &copy; <?php echo date("Y"); ?>
+	<p>Website designed and coded by Jacqueline Quintanilla. &copy; <?php echo date("Y"); ?>
 	</p>
 </footer>
 
 
-<!--   <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+   <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
         <script src="js/plugins.js"></script>
-        <script src="js/main.js"></script> -->
+        <script src="js/main.js"></script>
 
 <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. UA-93102704-1 -->
 <script>
