c0affb4d1cad0f2a46261a2436551381adbf7fe2
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2017-04-13 00:22:10 -0500

    more link modifying

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php $title="Design + Web Development";
 $pageClass="index";
 $pageDesc="A straightforward, strategic designer and web developer who loves working with the movers, shakers, and do-gooders.";
-$socialPic="img/jq-fbpreview.gif";
+$socialPic="img/jq-fbpreview-960.jpg";
 $pageLink= (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 ?>
 
