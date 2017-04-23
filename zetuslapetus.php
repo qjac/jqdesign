920f6ef6c421ec978ff427a39ba3be927eff3bed
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2017-04-23 00:31:19 -0500

    created zetus lapetus page. added content.

---
+++ zetuslapetus.php
@@ -0,0 +1,98 @@
+<?php $title="Zetus Lapetus";
+		$pageClass="portfolio-item zetus";
+		$pageDesc="VR for Everyone";
+$fbPic="http://jqdesign.co/img/zl/zl-logo-1920.png";
+$twitterPic="http://jqdesign.co/img/zl/zl-logo-1920.png";
+		$pageLink= (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+?>
+
+<?php include ("header.php"); ?>
+
+        <main class="content">
+	       <section class="content-top">
+<!--		       h1 is for project title-->
+		       <h1>Zetus Lapetus</h1>
+		       <hr>
+	       </section>
+
+	        <section class="project-intro">
+		        <h2>A future to envision.</h2>
+		        <p>Virtual Reality technology is moving fast and Zetus Lapetus wants to get you in the game. By offering a more affordable VR headset that works with both Android and iOS, Zetus Lapetus brings virtual reality to everyday life.</p>
+		        <div class="btn"><a href="/zetus-lapetus" class="hvr-icon-forward">Visit Site</a></div>
+	        </section>
+
+	        <section class="project-img">
+		        <div class="row">
+
+			        <img sizes="100vw" srcset="img/zl/zl-web-480.png 480w, img/zl/zl-web-960.png 960w, img/zl/zl-web-1920.png 1920w" src="img/zl/zl-web-960.png" alt="Zetus Lapetus website on laptop" class="full-width">
+		        </div>
+	        </section>
+
+	        <section class="project-img">
+		        <div class="row">
+			        <img sizes="100vw" srcset="img/zl/zl-moodboard-480.png 480w, img/zl/zl-moodboard-960.png 960w, img/zl/zl-moodboard-1920.png 1920w" src="img/zl/zl-moodboard-960.png" alt="moodboard" class="full-width">
+		        </div>
+	        </section>
+
+	        <section class="project-intro">
+		        <h3>VR for everyone.</h3>
+		        <p>After diving into market research and analyzing competing product branding (below left), a few key ideas emerged. As a product aimed at opening the world of VR to everyone for more than just games, inclusivity was vital. Two directions were moodboarded (below and above). Both aimed to take VR away from the sterility of tech and make it feel more comfortable, familiar, and social while still evoking the cutting edge nature of the technology.
+		        </p>
+		        <p>One kept close to the color palette common amongst competitors, but warmed it up a bit and paired it with elegant and inviting sans serif typefaces for a chic and modern vibe.</p>
+		        <p>The other option is brighter with a palette that diverges boldly from the competitors. This brighter palette was paired with type that has subtle tech geometrics to ground the social party vibe in the futurism of VR and technology. The vibrant, edgier option was chosen to reach a slightly younger, slightly wilder audience.</p>
+		        <div class="btn"><a href="img/zl/zl-styleguide.pdf" class="hvr-icon-forward">See the research, planning, and wireframes</a></div>
+	        </section>
+
+	        <section class="project-img">
+
+		        <div class="row">
+			        <h3 class=""><i class="fa fa-level-down fa-flip-horizontal" aria-hidden="true"></i>  Competitor Study  /</h3>
+			        <h3>/  Alternate Direction  <i class="fa fa-level-down" aria-hidden="true"></i></h3>
+		        </div>
+		        <div class="row">
+			        <img sizes="50vw" srcset="img/zl/zl-research-competitors-480.png 480w, img/zl/zl-research-competitors-960.png 960w, img/zl/zl-research-competitors-1920.png 1920w" src="img/zl/zl-research-competitors-960.png" alt="Cool Cat Crafts logo" class="half-width">
+			        <img sizes="50vw" srcset="img/zl/zl-research-moodboard-480.png 480w, img/zl/zl-research-moodboard-960.png 960w, img/zl/zl-research-moodboard-1920.png 1920w" src="img/zl/zl-research-moodboard-960.png" alt="Cool Cat Crafts logo" class="half-width">
+		        </div>
+	        </section>
+
+	        <section class="project-intro">
+		        <h3>Final Brand Elements</h3>
+	        </section>
+
+	        <section class="project-img">
+		        <div class="row">
+			        <img sizes="100vw" srcset="img/zl/zl-elements-480.png 480w, imgzl/zl-elements-960.png 960w, img/zl/zl-elements-1920.png 1920w" src="img/zl/zl-elements-960.png" alt="Meow!" class="full-width">
+		        </div>
+	        </section>
+
+	        <section class="project-intro">
+		        <p>
+			        Designed and developed in collaboration with:       </p>
+
+			        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> <a href="http://www.erikacrea.com">Erika Herrera</a></p>
+			        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> <a href="http://kalavisual.com">Kelsey Roenau</a></p>
+			        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> <a href="http://www.tomsolerportfolio.com">Tom Soler</a></p>
+			        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> <a href="http://www.jessewollin.com">Jesse Wollin</a></p>
+
+
+		        <p>My role: market research, moodboard exploration, type and color palette selection, and web development.</p>
+	        </section>
+
+	        <section class="project-img">
+		        <div class="row">
+			        <img sizes="100vw" srcset="img/zl/zl-logo-480.png 480w, imgzl/zl-logo--960.png 960w, img/zl/zl-logo-1920.png 1920w" src="img/zl/zl-logo-960.png" alt="Meow!" class="full-width">
+		        </div>
+	        </section>
+
+	        <section class="project-intro">
+		        <hr>
+		        <p class="category">Market Research // identity development // Web Design // Front End Development</p>
+<!--		        <p>Tools used: Adobe Illustrator + Adobe Animate + WordPress</p>-->
+		        <hr>
+	        </section>
+
+        </main>
+
+        <?php include ("footer.php"); ?>
+
+<!--    body closes in footer.php-->
