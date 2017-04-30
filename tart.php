6f95876188eca5a29d6b4bbe1688ef7302573712
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2017-04-30 11:34:16 -0500

    created tart.php. added content. created new img style for masonry display

---
+++ tart.php
@@ -0,0 +1,81 @@
+<?php $title="Tart";
+		$pageClass="portfolio-item tart";
+		$pageDesc="An in-your-face site for eyewear with attitude.";
+$fbPic="http://jqdesign.co/img/tart/tart-web-1920.png";
+$twitterPic="http://jqdesign.co/img/tart/tart-web-1920.png";
+		$socialPic="img/tart/tart-web-960.png";
+		$pageLink= (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+?>
+
+<?php include ("header.php"); ?>
+
+        <main class="content">
+	       <section class="content-top">
+<!--		       h1 is for project title-->
+		       <h1>Eyewear with Attitude</h1>
+		       <hr>
+	       </section>
+
+	        <section class="project-intro">
+
+		        <h2>Pucker up!</h2>
+		        <p>Tart is an online shop that specializes in eyewear for the unique, the stylish, and the rebellious. It needed a website that packed a punch while getting customers quickly to their perfect frame. Using a previously built custom content management system, the website was built using PHP and MySQL to organize Tart’s extensive collection of fierce frames.</p>
+
+		        <div class="btn"><a href="http://jacquelinequintanilla.com/webi2012/final/" class="hvr-icon-forward">Visit Site </a></div>
+	        </section>
+
+	        <section class="project-img">
+		        <div class="row">
+			        <img sizes="100vw" srcset="img/tart/tart-web-192.png 192w, img/tart/tart-web-480.png 480w, img/ec/ec-logo-960.png 960w, img/tart/tart-web-1920.png 1920w" src="img/tart/tart-web-960.png" alt="Emerald City logo" class="full-width">
+		        </div>
+<!--		        end row-->
+	        </section>
+
+	        <section class="project-intro">
+
+		        <h3>Digital Ads</h3>
+		        <p>Sized just right for Google.</p>
+	        </section>
+
+	        <section class="project-img">
+<!--		        <div class="row">-->
+<!--			        <img src="img/tart/animate-leaderboard.gif" alt="digital ad" class="no-stretch">-->
+<!--		        </div>-->
+		        <div class="row">
+			        <div class="masonry">
+				        <img src="img/tart/tart-ad-mobilebanner.png" alt="digital ad" class="no-stretch">
+				        <img src="img/tart/tart-ad-large.png" alt="digital ad" class="no-stretch">
+				        <img src="img/tart/tart-ad-halfpage.png" alt="digital ad" class="no-stretch">
+			        </div>
+		        </div>  <!--		        end row-->
+	        </section>
+
+	        <section class="project-img">
+		        <div class="row">
+			        <img sizes="100vw" srcset="img/tart/tart-logo-480.png 480w, img/tart/tart-logo-960.png 960w, img/tart/tart-logo-1920.png 1920w" src="img/tart/tart-logo-960.png" alt="Tart logo" class="full-width">
+		        </div> <!--		        end row-->
+	        </section>
+	        <section class="project-intro">
+		        <h3>Bold + Beautiful</h3>
+		        <p>The frames are bold, stylish, and definitely untraditional, so the identity needed to live up to the audacity. The wordmark is thick and gritty and off-kilter to evoke a punk don’t-give-a-damn vibe. The color palette is abrasive—stark white, deep black, bright yellow, and screaming red. The stock images selected are blazing with color and feature wild hair and rebellious attitude. That’s the in-your-face identity for this rowdy brand.</p>
+		        <hr>
+	        </section>
+	        <section class="project-img">
+		        <div class="row">
+			        <img sizes="100vw" srcset="img/tart/tart-moodboard-480.png 480w, img/tart/tart-moodboard-960.png 960w, img/tart/tart-moodboard-1920.png 1920w" src="img/tart/tart-moodboard-960.png" alt="moodboard" class="full-width">
+		        </div> <!--		        end row-->
+	        </section>
+
+	        <section class="project-intro">
+		        <hr>
+		        <p class="category">identity // digital&nbsp;advertising // web development</p>
+<!--		        <p>Tools used: Adobe Illustrator + Adobe Animate + WordPress</p>-->
+		        <hr>
+	        </section>
+
+
+        </main>
+
+        <?php include ("footer.php"); ?>
+
+<!--    body closes in footer.php-->
