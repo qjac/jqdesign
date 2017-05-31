edb4775f9f76fbe5ce66a31ccb83bbeddb07b0bd
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2017-05-31 11:35:00 -0500

    added .php to links to stop multiple choices error. updated resume.

--- index.php
+++ index.php
@@ -17,70 +17,70 @@ $pageLink= (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOS
         <section class="portfolio">
 
 	        <figure class="portfolio-item">
-		        <a href="/coolcatidentity" class="hvr-trim">
+		        <a href="/coolcatidentity.php" class="hvr-trim">
 			        <img sizes="100vw" srcset="img/ccc-meow-square-250.jpg 250w, img/ccc-meow-square-500.jpg 500w, img/ccc-meow-square-750.jpg 750w, img/ccc-meow-square-1000.jpg 1000w" src="img/ccc-meow-square-750.jpg" alt="meow" class="featureimg">
 		        </a>
 		        <figcaption>
 
 			        <h3>High-energy identity with an offbeat sensibility.</h3>
-			        <div class="btn "><a href="/coolcatidentity" class="hvr-icon-forward">Take a closer look!
+			        <div class="btn "><a href="/coolcatidentity.php" class="hvr-icon-forward">Take a closer look!
 				        </a></div>
 <!--		        </figcaption>-->
 <!--		        <p class="category">identity&nbsp;development // brand&nbsp;strategy // business&nbsp;collateral&nbsp;design</p>-->
 	        </figure>
 
 	        <figure class="portfolio-item">
-		        <a href="/coolcatdigital" class="hvr-trim">
+		        <a href="/coolcatdigital.php" class="hvr-trim">
 			        <img sizes="100vw" srcset="img/ccc-web-square-250.jpg 250w, img/ccc-web-square-500.jpg 500w, img/ccc-web-square-750.jpg 750w, img/ccc-web-square-1000.jpg 1000w" src="img/ccc-web-square-750.jpg" alt="website" class="featureimg">
 		        </a>
 		        <figcaption>
 			        <h3>Online tools for selling + connecting.</h3>
-			        <div class="btn "><a href="/coolcatdigital" class="hvr-icon-forward">Take a closer look! </a></div>
+			        <div class="btn "><a href="/coolcatdigital.php" class="hvr-icon-forward">Take a closer look! </a></div>
 			        <!--		        </figcaption>-->
 			        <!--		        <p class="category">identity&nbsp;development // brand&nbsp;strategy // business&nbsp;collateral&nbsp;design</p>-->
 	        </figure>
 
 	        <figure class="portfolio-item">
-		        <a href="/coolcatpackage" class="hvr-trim">
+		        <a href="/coolcatpackage.php" class="hvr-trim">
 			        <img sizes="100vw" srcset="img/ccc-package-square-250.jpg 250w, img/ccc-package-square-500.jpg 500w, img/ccc-package-square-750.jpg 750w, img/ccc-package-square-1000.jpg 1000w" src="img/ccc-package-square-750.jpg" alt="package with coasters" class="featureimg">
 		        </a>
 		        <figcaption>
 			        <h3>A box that's great in the store and better at home.</h3>
-			        <div class="btn "><a href="/coolcatpackage" class="hvr-icon-forward">Take a closer look! </a></div>
+			        <div class="btn "><a href="/coolcatpackage.php" class="hvr-icon-forward">Take a closer look! </a></div>
 			        <!--		        </figcaption>-->
 			        <!--		        <p class="category">identity&nbsp;development // brand&nbsp;strategy // business&nbsp;collateral&nbsp;design</p>-->
 	        </figure>
 
 	        <figure class="portfolio-item">
-		        <a href="/emeraldcity" class="hvr-trim">
+		        <a href="/emeraldcity.php" class="hvr-trim">
 			        <img sizes="100vw" srcset="img/ec-e-square-250.png 250w, img/ec-e-square-500.png 500w, img/ec-e-square-750.png 750w, img/ec-e-square-1000.png 1000w" src="img/ec-e-square-750.png" alt="Emerald City icon" class="featureimg">
 		        </a>
 		        <figcaption>
 			        <h3>The neighborhood cafe with a kick gets a welcoming identity.</h3>
-			        <div class="btn "><a href="/emeraldcity" class="hvr-icon-forward">Take a closer look! </a></div>
+			        <div class="btn "><a href="/emeraldcity.php" class="hvr-icon-forward">Take a closer look! </a></div>
 			        <!--		        </figcaption>-->
 			        <!--		        <p class="category">identity&nbsp;development // brand&nbsp;strategy // business&nbsp;collateral&nbsp;design</p>-->
 	        </figure>
 
 	        <figure class="portfolio-item">
-		        <a href="/zetuslapetus" class="hvr-trim">
+		        <a href="/zetuslapetus.php" class="hvr-trim">
 			        <img sizes="100vw" srcset="img/zl-icon-square-250.jpg 250w, img/zl-icon-square-500.jpg 500w, img/zl-icon-square-750.jpg 750w, img/zl-icon-square-1000.jpg 1000w" src="img/zl-icon-square-750.jpg" alt="zetus lapetus" class="featureimg">
 		        </a>
 		        <figcaption>
 
 			        <h3>Virtual reality for everyone.</h3>
-			        <div class="btn "><a href="/zetuslapetus" class="hvr-icon-forward">Take a closer look!
+			        <div class="btn "><a href="/zetuslapetus.php" class="hvr-icon-forward">Take a closer look!
 				        </a></div>
 	        </figure>
 
 	        <figure class="portfolio-item">
-		        <a href="/tart" class="hvr-trim">
+		        <a href="/tart.php" class="hvr-trim">
 			        <img sizes="100vw" srcset="img/tart-square-250.jpg 250w, img/tart-square-500.jpg 500w, img/tart-square-750.jpg 750w, img/tart-square-1000.jpg 1000w" src="img/tart-square-750.jpg" alt="tart" class="featureimg">
 		        </a>
 		        <figcaption>
 
 			        <h3>An in-your-face identity and website for eyewear with attitude.</h3>
-			        <div class="btn "><a href="/tart" class="hvr-icon-forward">Take a closer look!
+			        <div class="btn "><a href="/tart.php" class="hvr-icon-forward">Take a closer look!
 				        </a></div>
 	        </figure>
 
