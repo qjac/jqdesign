0b11e4dc03ed89ac957b4c08128072110a40e976
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-02-15 16:16:20 -0600

    mid build on jq brand page

--- jqdesignbrand.php
+++ jqdesignbrand.php
@@ -49,9 +49,8 @@ $twitterPic="http://jqdesign.co/img/zl/zl-logo-1920.png";
 	        </section>
 
 	        <section class="project-intro">
-		        <h3>Sass it up.</h3>
+		        <h3>More sass!</h3>
 		        <p>In researching and rethinking the previous brand, one thing was certain. It needed more sass.</p>
-		        <p>The familiar slab serif from the previous identity is still in play, but in a more nuanced way. The palette moved from intriguing, but demure and serious, to bright and audacious with enough navy to keep it professional. The logo moved from delicate to solid. And that swash on the Q is a thing of beauty—full stop.</p>
 	        </section>
 
 	        <section class="project-img">
@@ -62,63 +61,48 @@ $twitterPic="http://jqdesign.co/img/zl/zl-logo-1920.png";
 		        	<img sizes="50vw" srcset="img/jq/jqbrand-sketch1-480.png 480w, img/jq/jqbrand-sketch1-960.png 960w, img/jq/jqbrand-sketch1-1920.png 1920w" src="img/jq/jqbrand-sketch1-960.png" alt="preliminary logo sketches and notes" class="half-width">
 			        <img sizes="50vw" srcset="img/jq/jqbrand-sketch2-480.png 480w, img/jq/jqbrand-sketch2-960.png 960w, img/jq/jqbrand-sketch2-1920.png 1920w" src="img/jq/jqbrand-oldbizcard-960.png" alt="preliminary logo sketches and notes" class="half-width">
 		        </div>
-</section>
-		        <section class="project-intro">
-		        <hr>
-	        
-		     
+		      </section>
+
+	        <section class="project-img">
 		        <div class="row">
-			        <h3 class=""><i class="fa fa-level-down fa-flip-horizontal" aria-hidden="true"></i>&nbsp; Prior Branding  &nbsp;/<h3>
-			        <h3>/&nbsp;  New Direction &nbsp;<i class="fa fa-level-down" aria-hidden="true"></i></h3>
+			        <h3 class=""><i class="fa fa-level-down fa-flip-horizontal" aria-hidden="true"></i>  Prior Branding  /</h3>
+			        <h3>/  New Preliminary Comps  <i class="fa fa-level-down" aria-hidden="true"></i></h3>
 		        </div>
 		        <div class="row">
 			        <img sizes="50vw" srcset="img/jq/jqbrand-oldbizcard-480.png 480w, img/jq/jqbrand-oldbizcard-960.png 960w, img/jq/jqbrand-oldbizcard-1920.png 1920w" src="img/jq/jqbrand-oldbizcard-960.png" alt="competitor research" class="half-width">
-			        
-			        <img sizes="50vw" srcset="img/jq/jqbrand-oldbizcard-480.png 480w, img/jq/jqbrand-oldbizcard-960.png 960w, img/jq/jqbrand-oldbizcard-1920.png 1920w" src="img/jq/jqbrand-oldbizcard-960.png" alt="competitor research" class="half-width">
+			        <img sizes="50vw" srcset="img/jq/jqbrand-artboard-480.png 480w, img/jq/jqbrand-artboard-960.png 960w, img/jq/jqbrand-artboard-1920.png 1920w" src="img/jq/jqbrand-artboard-960.png" alt="early versions of new branding" class="half-width">
 		        </div>
 	        </section>
 	        <section class="project-intro">
 		        <h3>Final Brand Elements</h3>
+		        <p>The familiar slab serif from the previous identity is still in play, but in a more nuanced way. The palette moved from intriguing, but demure and serious, to bright and audacious with enough navy to keep it professional. The logo moved from delicate to solid. And that swash on the Q is a thing of beauty—full stop.</p>
 	        </section>
 	        <section class="project-img">
 		        <div class="row">
-			        <img sizes="100vw" srcset="img/jq/jqbrand-moodboard-480.png 480w, img/jq/jqbrand-moodboard-960.png 960w, img/jq/jqbrand-moodboard-1920.png 1920w" src="img/jq/jqbrand-moodboard-960.png" alt="final brand elements" class="full-width">
+			        <img sizes="100vw" srcset="img/jq/jqbrand-moodard-480.png 480w, img/jq/jqbrand-moooard-960.png 960w, img/jq/jqbrand-odboard-1920.png 1920w" src="img/jq/jqbrand-moboard-960.png" alt="final brand elements" class="full-width">
 		        </div>
-		      </section>
+		      <div class="row">
+			        <img sizes="50vw" srcset="img/jq/jqbrand-boothmockup-480.png 480w, img/jq/jqbrand-boothmockup-960.png 960w, img/jq/jqbrand-boothmockup-1920.png 1920w" src="img/jq/jqbrand-boothmockup-960.png" alt="mockup for portfolio show booth" class="half-width">
+			        <img sizes="50vw" srcset="img/jq/jqbrand-bizcard-480.png 480w, img/jq/jqbrand-bizcard-960.png 960w, img/jq/jqbrand-bizcard-1920.png 1920w" src="img/jq/jqbrand-bizcard-960.png" alt="final business card" class="half-width">
+		        </div>
+	        </section>
 
 		      <section class="project-intro">
 	        <h3></h3>
 	        <p>In today's digital world, a killer website is key. </p>
 	        <p></p>
 	        <div class="btn"><a href="/" class="hvr-icon-forward">You're already here...Check out more of my work</a></div>
-		      
-<!-- ????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????? -->
-
-<section class="project-intro">
-		        <p>Spreading glitter and glee is central to Cool Cat Crafts’ mission. As an independent, handmade shop, sharing joy with supporters and customers is the best way to meet that mission. To that end, package inserts that delight and colorful business cards to spread the word are a must.</p>
-		        <hr>
-	        </section>
-	        <div class="row">
-			        <img sizes="100vw" srcset="img/jq/jqbrand-bizcard-480.png 480w, img/jq/jqbrand-bizcard-960.png 960w, img/jq/jqbrand-bizcard-1920.png 1920w" src="img/jq/jqbrand-bizcard-960.png" alt="JQ Design Co. business cards" class="full-width">
+	      </section>
+	        <section class="project-img">
+	        	<div class="row">
+			        <img sizes="50vw" srcset="img/jq/jqbrand-sketch3-480.png 480w, img/jq/jqbrand-sketch3-960.png 960w, img/jq/jqbrand-sketch3-1920.png 1920w" src="img/jq/jqbrand-sketch3-960.png" alt="website design sketches" class="half-width">
+			        <img sizes="50vw" srcset="img/jq/jqbrand-artbard-480.png 480w, img/jq/jqbrand-artoard-960.png 960w, img/jq/jqbrand-arboard-1920.png 1920w" src="img/jq/jqbrand-artbard-960.png" alt="website mockup" class="half-width">
 		        </div>
-
-	        <section class="project-intro">
-		        <h3>text</h3>
-		        <p>more text
-		        </p>
-		        <div class="btn"><a href="img/zl/zl-styleguide.pdf" class="hvr-icon-forward">See the research, planning, and wireframes</a></div>
-	        </section>
-	        <section class="project-intro">
-		        <p>
-			        Designed and developed in collaboration with:       </p>
-			        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> <a href="http://www.erikacrea.com">Erika Herrera</a></p>
-			        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> <a href="http://kalavisual.com">Kelsey Roenau</a></p>
-			        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> <a href="http://www.tomsolerportfolio.com">Tom Soler</a></p>
-			        <p><i class="fa fa-chevron-right" aria-hidden="true"></i> <a href="http://www.jessewollin.com">Jesse Wollin</a></p>
-		        <p>My role: market research, moodboard exploration, type and color palette selection, and web development.</p>
+	        	<div class="row">
+			        <img sizes="100vw" srcset="img/jq/jqbrand-moodard-480.png 480w, img/jq/jqbrand-moooard-960.png 960w, img/jq/jqbrand-odboard-1920.png 1920w" src="img/jq/jqbrand-moboard-960.png" alt="JQ banner" class="full-width">
+		        </div>
 	        </section>
 
-	       <!-- keep the rest -->
 	        <section class="project-intro">
 		        <hr>
 	        </section>
