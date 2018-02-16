4c470e4c87fd20d593f99e6f22d954b732c6ecb1
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-02-15 19:24:18 -0600

    mid build save

--- jqdesignbrand.php
+++ jqdesignbrand.php
@@ -38,7 +38,7 @@ $twitterPic="http://jqdesign.co/img/zl/zl-logo-1920.png";
 
 	        <section class="project-intro">
 		        <h2>Whoa. Who's that?!</h2>
-		        <p>On the crowded floor of the portfolio showcase. At a busy AIGA event. On the screen of a harried hiring manager. In the overflowing win-a-free-burrito bowl. That's what I wanted to hear.</p>
+		        <p>On the crowded floor of the portfolio showcase. At a busy AIGA event. On the computer screen of a harried hiring manager. In the overflowing win-a-free-burrito jar. I wanted to make people notice.</p>
 		        <p>As a new designer hitting the Twin Citie's scene, I needed to grab attention and keep it. All while being authentic to who I am. So here we are. A bold palette. Humble type with a hint of the dramatic. And a punch of sass.</p>
 	        </section>
 
@@ -49,13 +49,13 @@ $twitterPic="http://jqdesign.co/img/zl/zl-logo-1920.png";
 	        </section>
 
 	        <section class="project-intro">
-		        <h3>More sass!</h3>
-		        <p>In researching and rethinking the previous brand, one thing was certain. It needed more sass.</p>
+		        <h3>From blah to BLAM!</h3>
+		        <p>In researching and rethinking the previous brand, one thing was certain. It needed more boldness. More intensity. More fun. And definitely more sass.</p>
 	        </section>
 
 	        <section class="project-img">
 		        <div class="row">
-			        <img sizes="100vw" srcset="img/jq/jqbrand-moodboard-480.png 480w, img/jq/jqbrand-moodboard-960.png 960w, img/jq/jqbrand-moodboard-1920.png 1920w" src="img/jq/jqbrand-moodboard-960.png" alt="moodboard and initial brand direction" class="full-width">
+			        <img sizes="100vw" srcset="img/jq/jqbrand-moodboard-480.png 480w, img/jq/jqbrand-moodboard-960.png 960w, img/jq/jqbrand-moodboard-1920.png 1920w" src="img/jq/jqbrand-moodboard-960.png" alt="moodboard and initial brand direction. strategic. collaborative. sassy. My mission is to thoughtfully craft experiences that clarify complex problems and bring charm to the everyday." class="full-width">
 		        </div>
 		        <div class="row">
 		        	<img sizes="50vw" srcset="img/jq/jqbrand-sketch1-480.png 480w, img/jq/jqbrand-sketch1-960.png 960w, img/jq/jqbrand-sketch1-1920.png 1920w" src="img/jq/jqbrand-sketch1-960.png" alt="preliminary logo sketches and notes" class="half-width">
@@ -66,7 +66,7 @@ $twitterPic="http://jqdesign.co/img/zl/zl-logo-1920.png";
 	        <section class="project-img">
 		        <div class="row">
 			        <h3 class=""><i class="fa fa-level-down fa-flip-horizontal" aria-hidden="true"></i>  Prior Branding  /</h3>
-			        <h3>/  New Preliminary Comps  <i class="fa fa-level-down" aria-hidden="true"></i></h3>
+			        <h3>/  Preliminary Drafts  <i class="fa fa-level-down" aria-hidden="true"></i></h3>
 		        </div>
 		        <div class="row">
 			        <img sizes="50vw" srcset="img/jq/jqbrand-oldbizcard-480.png 480w, img/jq/jqbrand-oldbizcard-960.png 960w, img/jq/jqbrand-oldbizcard-1920.png 1920w" src="img/jq/jqbrand-oldbizcard-960.png" alt="competitor research" class="half-width">
@@ -75,34 +75,40 @@ $twitterPic="http://jqdesign.co/img/zl/zl-logo-1920.png";
 	        </section>
 	        <section class="project-intro">
 		        <h3>Final Brand Elements</h3>
-		        <p>The familiar slab serif from the previous identity is still in play, but in a more nuanced way. The palette moved from intriguing, but demure and serious, to bright and audacious with enough navy to keep it professional. The logo moved from delicate to solid. And that swash on the Q is a thing of beauty—full stop.</p>
+		        <p>The familiar slab serif from the previous identity is still in play, but in a more nuanced way. The palette moved from intriguing, but demure and serious, to bright and audacious with enough navy to keep it professional. The logo moved from delicate to solid. And that swash on the Q is a thing of beauty&mdash;full stop.</p>
 	        </section>
 	        <section class="project-img">
 		        <div class="row">
 			        <img sizes="100vw" srcset="img/jq/jqbrand-moodard-480.png 480w, img/jq/jqbrand-moooard-960.png 960w, img/jq/jqbrand-odboard-1920.png 1920w" src="img/jq/jqbrand-moboard-960.png" alt="final brand elements" class="full-width">
 		        </div>
 		      <div class="row">
-			        <img sizes="50vw" srcset="img/jq/jqbrand-boothmockup-480.png 480w, img/jq/jqbrand-boothmockup-960.png 960w, img/jq/jqbrand-boothmockup-1920.png 1920w" src="img/jq/jqbrand-boothmockup-960.png" alt="mockup for portfolio show booth" class="half-width">
-			        <img sizes="50vw" srcset="img/jq/jqbrand-bizcard-480.png 480w, img/jq/jqbrand-bizcard-960.png 960w, img/jq/jqbrand-bizcard-1920.png 1920w" src="img/jq/jqbrand-bizcard-960.png" alt="final business card" class="half-width">
+		      	<img sizes="50vw" srcset="img/jq/jqbrand-bizcard-480.png 480w, img/jq/jqbrand-bizcard-960.png 960w, img/jq/jqbrand-bizcard-1920.png 1920w" src="img/jq/jqbrand-bizcard-960.png" alt="final business card" class="half-width">
+			        <!-- <img sizes="50vw" srcset="img/jq/jqbrand-boothmockup-480.png 480w, img/jq/jqbrand-boothmockup-960.png 960w, img/jq/jqbrand-boothmockup-1920.png 1920w" src="img/jq/jqbrand-boothmockup-960.png" alt="mockup for portfolio show booth" class="half-width"> -->
+			        <img sizes="50vw" srcset="img/jq/jqbrand-banner-480.png 480w, img/jq/jqbrand-banner-960.png 960w, img/jq/jqbrand-banner-1920.png 1920w" src="img/jq/jqbrand-banner-960.png" alt="JQ banner" class="half-width">
+			        <!-- <img sizes="50vw" srcset="img/jq/jqbrand-print-480.png 480w, img/jq/jqbrand-print-960.png 960w, img/jq/jqbrand-print-1920.png 1920w" src="img/jq/jqbrand-print-960.png" alt="printing portfolio show items" class="half-width"> -->
 		        </div>
+		        <!-- <div class="row">
+			        <img sizes="100vw" srcset="img/jq/jqbrand-bizcard-480.png 480w, img/jq/jqbrand-bizcard-960.png 960w, img/jq/jqbrand-bizcard-1920.png 1920w" src="img/jq/jqbrand-bizcard-960.png" alt="final business card" class="full-width">
+		        </div> -->
 	        </section>
 
 		      <section class="project-intro">
 	        <h3></h3>
-	        <p>In today's digital world, a killer website is key. </p>
+	        <p>In today's digital world, a killer website is key. The home page blasts you with in-your-face color and leads you right to the work. The interior pages are calmer with a soothing blue gradient that fades into a white background allowing you to focus on the what's important&mdash;the content.</p>
 	        <p></p>
-	        <div class="btn"><a href="/" class="hvr-icon-forward">You're already here...Check out more of my work</a></div>
+	        <div class="btn"><a href="/" class="hvr-icon-forward">You're already here…Check out more of my work</a></div>
 	      </section>
 	        <section class="project-img">
 	        	<div class="row">
-			        <img sizes="50vw" srcset="img/jq/jqbrand-sketch3-480.png 480w, img/jq/jqbrand-sketch3-960.png 960w, img/jq/jqbrand-sketch3-1920.png 1920w" src="img/jq/jqbrand-sketch3-960.png" alt="website design sketches" class="half-width">
-			        <img sizes="50vw" srcset="img/jq/jqbrand-artbard-480.png 480w, img/jq/jqbrand-artoard-960.png 960w, img/jq/jqbrand-arboard-1920.png 1920w" src="img/jq/jqbrand-artbard-960.png" alt="website mockup" class="half-width">
-		        </div>
-	        	<div class="row">
-			        <img sizes="100vw" srcset="img/jq/jqbrand-moodard-480.png 480w, img/jq/jqbrand-moooard-960.png 960w, img/jq/jqbrand-odboard-1920.png 1920w" src="img/jq/jqbrand-moboard-960.png" alt="JQ banner" class="full-width">
+			        <img sizes="100vw" srcset="img/jq/jqbrand-web-480.png 480w, img/jq/jqbrand-web-960.png 960w, img/jq/jqbrand-web-1920.png 1920w" src="img/jq/jqbrand-web-960.png" alt="website mockup" class="full-width">
 		        </div>
 	        </section>
 
+	        	<!-- <div class="row">
+	        		<img sizes="50vw" srcset="img/jq/jqbrand-sketch3-480.png 480w, img/jq/jqbrand-sketch3-960.png 960w, img/jq/jqbrand-sketch3-1920.png 1920w" src="img/jq/jqbrand-sketch3-960.png" alt="website design sketches" class="half-width">
+			        
+		        </div>
+ -->
 	        <section class="project-intro">
 		        <hr>
 	        </section>
