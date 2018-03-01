58f6c5070e1fcb9a593980484e56382673bca947
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-03-01 17:02:29 -0600

    opps. forgot to update the header variables

--- jqdesignbrand.php
+++ jqdesignbrand.php
@@ -1,8 +1,8 @@
 <?php $title="JQ Design Co.";
 		$pageClass="portfolio-item jqdesign";
-		$pageDesc="VR for Everyone";
-$fbPic="http://jqdesign.co/img/zl/zl-logo-1920.png";
-$twitterPic="http://jqdesign.co/img/zl/zl-logo-1920.png";
+		$pageDesc="A portfolio with punch.";
+$fbPic="http://jqdesign.co/img/jq/jqbrand-web-1920.png";
+$twitterPic="http://jqdesign.co/img/jq/jqbrand-web-1920.png";
 		$pageLink= (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 ?>
 
