70b86ef193bfb49ffa24afcef1d74a5e86a1a182
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-03-11 12:32:19 -0500

    js countdown only on about page

--- footer.php
+++ footer.php
@@ -1,4 +1,3 @@
-
 <footer>
 	<h4>Let's chat!</h4>
 
@@ -17,8 +16,7 @@
 			<i class="fab fa-linkedin-in fa-2x" aria-hidden="true"></i></a>
 	</div>
 
-	<p>Website designed and coded by Jacqueline Quintanilla. &copy; <?php echo date("Y"); ?>
-	</p>
+	<p>Website designed and coded by Jacqueline Quintanilla. &copy; <?php echo date("Y"); ?></p>
 </footer>
 
 
@@ -35,21 +33,21 @@
 		e.src='https://www.google-analytics.com/analytics.js';
 		r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
 	ga('create','UA-93102704-1','auto');ga('send','pageview');
-</script>
+</script> <!-- end google analytics -->
 
-<script> // modified from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_countdown
+<script>
+	// modified from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_countdown
+$(document).ready(function() {
+      if (window.location.pathname == '/about.php') {
 // Set the date we're counting down to
 // 8/23/2018 at 6am is when the fair kicks off
 var countDownDate = new Date("Aug 23, 2018 06:00:00").getTime();
-
 // Update the count down every 1 second
 var x = setInterval(function() {
   // Get todays date and time
   var now = new Date().getTime();
-
   // Find the distance between now an the count down date
   var distance = countDownDate - now;
-
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
@@ -65,6 +63,8 @@ var x = setInterval(function() {
     document.getElementById("fair-countdown").innerHTML = "";
   }
 }, 1000);
-</script>
+      } // end if (window.location.pathname == '/about.php')
+    } ); // end $(document).ready(function()
+</script>  <!-- end countdown script -->
 </body>
 </html>
