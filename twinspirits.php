d21901a844b3981cf0ad9d988c7a913352f4ded4
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-04-17 17:33:50 -0500

    add photos to tsd project

--- twinspirits.php
+++ twinspirits.php
@@ -25,49 +25,30 @@ $twitterPic="http://jqdesign.co/img/jq/jqbrand-web-1920.png";
 	       </section>
 
 	        <section class="project-intro">
-		        <h2>Quench your thirst with WordPress</h2>
+		        <h2>WordPress quenches thirst</h2>
 		        <p>A local distillery was looking for a few new features and a refresh on the look of their website. Custom fields, post types, and page templates extend the original WordPress design to get Twin Spirits Distillery customers the info they need and provide the client with better admin tools for managing content. Modern CSS techniques ensure the layout stays functional and looks professional regardless of how it's being viewed. Now, that's something to raise a glass to&mdash;Cheers!</p>
-		        <p>Web design by <a href="http://janinahartleydesigns.com/">Janina Hartley</a>. <br>
-		        	Branding by <a href="https://evologycreative.com/work/twin-spirits-distillery/">Evology</a>.</p>
 
 		        <div class="btn"><a href="https://www.twinspirits.us/" class="hvr-icon-forward">Visit Site <i class="fas fa-angle-double-right fa-lg" data-fa-transform="down-1" aria-hidden="true"></i></a></div>
-
-	        </section>
-
-<!-- 	        <section class="project-img">
-		        <div class="row">
-			        <img sizes="100vw" srcset="img/jq/jqbrand-sticker-480.png 480w, img/jq/jqbrand-sticker-960.png 960w, img/jq/jqbrand-sticker-1920.png 1920w" src="img/jq/jqbrand-sticker-960.png" alt="stickers with logo" class="full-width">
-		        </div>
-	        </section>
-
-	        <section class="project-intro">
-		        <h3>From blah to BLAM!</h3>
-		        <p>In researching and rethinking the previous brand, one thing was certain. It needed more boldness. More intensity. More fun. And definitely more sass.</p>
 	        </section>
 
 	        <section class="project-img">
-		        <div class="row">
-			        <img sizes="100vw" srcset="img/jq/jqbrand-moodboard-480.png 480w, img/jq/jqbrand-moodboard-960.png 960w, img/jq/jqbrand-moodboard-1920.png 1920w" src="img/jq/jqbrand-moodboard-960.png" alt="moodboard and initial brand direction. strategic. collaborative. sassy. My mission is to thoughtfully craft experiences that clarify complex problems and bring charm to the everyday." class="full-width">
+	        	<div class="row">
+			        <img sizes="100vw" srcset="img/tsd/tsd-booze-480.png 480w, img/tsd/tsd-booze-960.png 960w, img/tsd/tsd-booze-1920.png 1920w" src="img/tsd/tsd-booze-960.png" alt="product page (rum)" class="full-width">
 		        </div>
+		        
 		        <div class="row">
-		        	<img sizes="50vw" srcset="img/jq/jqbrand-sketch1-480.png 480w, img/jq/jqbrand-sketch1-960.png 960w, img/jq/jqbrand-sketch1-1920.png 1920w" src="img/jq/jqbrand-sketch1-960.png" alt="preliminary logo sketches and notes" class="half-width">
-			        <img sizes="50vw" srcset="img/jq/jqbrand-sketch2-480.png 480w, img/jq/jqbrand-sketch2-960.png 960w, img/jq/jqbrand-sketch2-1920.png 1920w" src="img/jq/jqbrand-oldbizcard-960.png" alt="preliminary logo sketches and notes" class="half-width">
+		        	<img sizes="50vw" srcset="img/tsd/tsd-desk-480.png 480w, img/tsd/tsd-desk-960.png 960w, img/tsd/tsd-desk-1920.png 1920w" src="img/tsd/tsd-desk-960.png" alt="desktop homepage" class="half-width">
+			        <img sizes="50vw" srcset="img/tsd/tsd-spread-480.png 480w, img/tsd/tsd-spread-960.png 960w, img/tsd/tsd-spread-1920.png 1920w" src="img/jq/tsd/tsd-spread-960.png" alt="multiple webpages" class="half-width">
+		        </div>
+
+		        <div class="row">
+			        <img sizes="100vw" srcset="img/tsd/tsd-mobile-480.png 480w, img/tsd/tsd-mobile-960.png 960w, img/tsd/tsd-mobile-1920.png 1920w" src="img/tsd/tsd-mobile-960.png" alt="homepage on mobile device" class="full-width">
 		        </div>
 		      </section>
-
-	        <section class="project-img">
-		        <div class="row">
-			        <h3 class=""><i class="fas fa-level-down-alt" data-fa-transform="flip-h down-6" aria-hidden="true"></i>  Prior Branding  </h3>
-			        <h3>&nbsp;&nbsp;//&nbsp;&nbsp;</h3>
-			        <h3>  Preliminary Drafts  <i class="fas fa-level-down-alt" data-fa-transform="down-6" aria-hidden="true"></i></h3>
-		        </div>
-		        <div class="row">
-			        <img sizes="50vw" srcset="img/jq/jqbrand-oldbizcard-480.png 480w, img/jq/jqbrand-oldbizcard-960.png 960w, img/jq/jqbrand-oldbizcard-1920.png 1920w" src="img/jq/jqbrand-oldbizcard-960.png" alt="competitor research" class="half-width">
-			        <img sizes="50vw" srcset="img/jq/jqbrand-artboard-480.png 480w, img/jq/jqbrand-artboard-960.png 960w, img/jq/jqbrand-artboard-1920.png 1920w" src="img/jq/jqbrand-artboard-960.png" alt="early versions of new branding" class="half-width">
-		        </div>
-	        </section> -->
-	        
-	       
+					<section class="project-intro">
+		        <p>Web design by <a href="http://janinahartleydesigns.com/">Janina Hartley</a>. <br>
+		        	Branding by <a href="https://evologycreative.com/work/twin-spirits-distillery/">Evology</a>.</p>
+	        </section>
 
 	        <section class="project-intro">
 		        <hr>
